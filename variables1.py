9627e520488139ab48f59644655d9b877a78d0ae
himnie <almorabitibrahim@gmail.com>
2025-04-10 18:29:33 +0200

    VARIABLES

---
+++ variables1.py
@@ -0,0 +1,25 @@
+#tipos enteros(sin decimales)
+edad=40
+#tipos con decimales se usa el punto para indicar decimales
+
+temperatura=20.7
+
+#cadenas de letras (string)
+
+nombre='Ana'
+apellido=("Pi i Margall")
+direccion =""" Calle del pino 08001 barcelona"""
+
+precio=100
+preciomal="100"
+cp="08001"
+precioConIva=precio*1.21
+precioConIva2=float(preciomal)*1.21  # aqui solo se convierte el numero
+print(precio*1.21)
+print(float(preciomal)*1.21)
+
+
+
+#Booleano valores de cierto/falso (bool)
+mayorDeEdad=True
+estaLogueado=False
